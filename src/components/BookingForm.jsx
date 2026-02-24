b621b731edebce3ee43d00717eb0e21c19a62376
sadidrahin.tzyu@gmail.com <mdsadidrahinnohan@sadidrahintzyugmailcoms-MacBook-Air.local>
2026-02-24 23:43:35 +0800

    again commit

--- src/components/BookingForm.jsx
+++ src/components/BookingForm.jsx
@@ -10,6 +10,7 @@ import { motion, AnimatePresence } from 'framer-motion';
 const BACKEND_URL = "https://elite-transport-backend.onrender.com";
 const stripePromise = loadStripe("pk_test_51Sx57rFVFmdbJGhjAMobcQfMvc9Ur2fWde1AilhYul33gzBMeLpUSWGWKYKzWX7FkNjRetx7e0Rf0DdINpMUWQ6800szEvZVwl");
 const MAPBOX_TOKEN = process.env.REACT_APP_MAPBOX_TOKEN;
+console.log("My token is loading: ", process.env.REACT_APP_MAPBOX_TOKEN);
 
 const FLEET_PRICING = {
   zones: {
